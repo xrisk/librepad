5ccd461e07ba679fb2d20f2ed80701cddd3894cc
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-23 19:56:40 +0530

    LULBA

---
+++ runner.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'PUT'])
+def run():
+    f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8")
+    return (f.stdout, {'Access-Control-Allow-Origin': '*'})
+
+if __name__ == "__main__":
+    app.run()
