cec6f7418039f67550c3e346cba62d2dc945e2e3
Rishav Kundu <rishav.kundu98@gmail.com>
2017-01-24 05:50:26 +0530

    a

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'PUT'])
 def run():
-    f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8")
+    f = subprocess.run(["python"], input=request.form["code"], stdout=subprocess.PIPE, encoding="utf-8", stderr=subprocess.STDOUT)
     return (f.stdout, {'Access-Control-Allow-Origin': '*'})
 
 if __name__ == "__main__":
